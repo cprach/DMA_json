c372b104b2707d36d38dc32e6ec429124f81da49
Rachael Colley <rachael.colley@computerpower.edu.au>
2015-02-16 18:23:22 +1100

    more

--- src/com/cp/dma/json/PlayerManager.java
+++ src/com/cp/dma/json/PlayerManager.java
@@ -3,13 +3,9 @@ package com.cp.dma.json;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PList {
+public class PlayerManager {
 	
 	List<Player> players = new ArrayList<Player>();
-	
-	public PList() {
-		
-	}
 
 	public List<Player> getPlayers() {
 		return players;
--- src/com/cp/dma/json/Start.java
+++ src/com/cp/dma/json/Start.java
@@ -7,11 +7,11 @@ public class Start {
 	public static void main(String[] args) {
 	
 		PlayerData pd = new PlayerData();
-		String s = pd.readData();
-		System.out.println(s);
+		String playerData = pd.readData();
+		System.out.println(playerData);
 		
 		Gson gson = new Gson();
-		PList p = gson.fromJson(s, PList.class);
+		PlayerManager p = gson.fromJson(playerData, PlayerManager.class);
 		
 		System.out.print(p.getPlayers().size());
 
